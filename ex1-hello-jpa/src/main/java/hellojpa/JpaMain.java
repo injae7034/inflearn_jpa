e0903044313dade2203010533391d0b5bb35621e
injae7034 <injae7034@gmail.com>
2022-07-04 16:53:52 +0900

    필드와 컬럼 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,15 +17,6 @@ public class JpaMain {
 
         try {
 
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
-
-            em.clear();
-
-            Member member2 = em.find(Member.class, 150L);
-
-            System.out.println("====================");
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
