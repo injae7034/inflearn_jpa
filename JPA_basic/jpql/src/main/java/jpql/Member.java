93202cb19e74c3eebf81a55488157da5beaeea7e
injae7034 <injae7034@gmail.com>
2022-07-05 18:07:20 +0900

    프로젝트 세팅

---
+++ JPA_basic/jpql/src/main/java/jpql/Member.java
@@ -0,0 +1,44 @@
+package jpql;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String userName;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
